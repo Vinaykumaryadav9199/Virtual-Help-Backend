24b67f31aefc773b1b3e06e71fd918a29311556d
Vinaykumaryadav9199 <132898839+Vinaykumaryadav9199@users.noreply.github.com>
2024-02-09 21:57:59 +0530

    Update  domain url change auth.js

--- Routes/auth.js
+++ Routes/auth.js
@@ -193,7 +193,7 @@ router.post("/Login" ,async(req, res)=>{
             //   });
 
             res.cookie('token', authToken, {
-                domain: 'virtual-help.vercel.app',
+                domain: '.virtual-help.vercel.app',
                 path: '/',
                 httpOnly: true,
                // secure: process.env.NODE_ENV === 'production', // Ensure HTTPS in production
